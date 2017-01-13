5ef923263c1bf6db4dcd57f53f47bb3c21691202
porchezhiyan <chezhiyanspartan@gmail.com>
2017-01-13 13:12:23 +0530

    call Json with button

--- app/src/main/java/com/example/harshu/jsonparse/MainActivity.java
+++ app/src/main/java/com/example/harshu/jsonparse/MainActivity.java
@@ -4,6 +4,8 @@ package com.example.harshu.jsonparse;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.android.volley.DefaultRetryPolicy;
@@ -13,7 +15,6 @@ import com.android.volley.Response;
 import com.android.volley.RetryPolicy;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
 import org.json.JSONObject;
@@ -26,6 +27,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        Button CALL = (Button)findViewById(R.id.CALL);
+
+
+    }
+    public void buttonOnClick(View view)
+    {
 
 
         int sockettimeout = 30000;
@@ -43,7 +51,7 @@ public class MainActivity extends AppCompatActivity {
         JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET,url,(String)null, new Response.Listener<JSONObject>() {
             @Override
             public void onResponse(JSONObject response) {
-                mTextView.setText("the response is:"+response.toString().substring(0,5500));
+                mTextView.setText("the response is:"+response.toString());
             }
         }, new Response.ErrorListener() {
             @Override
@@ -74,7 +82,6 @@ public class MainActivity extends AppCompatActivity {
         });*/
 
         queue.add(request);
-
 
     }
 
